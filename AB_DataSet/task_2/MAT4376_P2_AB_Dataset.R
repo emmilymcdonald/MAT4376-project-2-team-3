50b1abbe081f805104fb882fcb99e06faeeaea41
Emily McDonald <mcdonaldemily14@gmail.com>
2023-06-13 10:17:14 -0400

    Replaced old R file with updated version

--- AB_DataSet/task_2/MAT4376_P2_AB_Dataset.R
+++ AB_DataSet/task_2/MAT4376_P2_AB_Dataset.R
@@ -1,6 +1,7 @@
 #### ab_data set ####
-data1 <- ab_data_2_
+data1 <- ab_data_v2
 summary(data1)
+str(data1)
 
 # Define the observed data
 user_id <- data1$user_id
@@ -43,6 +44,17 @@ users <- data1$user_id
 as.data.frame(table(users)) %>%
   arrange(desc(Freq))
 
+
+##duplicates of users
+library(tidyverse)
+duplicated(data1$user_id)
+unique(data1$user_id)
+a <- sum(unique(data1$user_id))
+
+b <- sum(duplicated(data1$user_id))
+
+b/a
+
 # hist on landing page
 library(ggplot2)
 page <- data1$landing_page
@@ -71,35 +83,116 @@ conversion_rate_plot <- ggplot(data = data1, aes(x = landing_page, y = converted
 print(conversion_rate_plot)
 
 
-# time series plot on data? aggrgate time into smaller categories  (weeks?)
 
 
-#Task 2:
+# Calculate the conversion rate for the old page
+conversion_rate_old <- sum(data1$converted[data1$landing_encoded == 0]) / sum(data1$landing_encoded == 0)
 
-# Set the seed for reproducibility
-set.seed(123)
+# Calculate the conversion rate for the new page
+conversion_rate_new <- sum(data1$converted[data1$landing_encoded == 1]) / sum(data1$landing_encoded == 1)
 
-# Compute the conversion rates for the old and new landing pages
-old_conversion_rate <- sum(converted[landing_page == "old_page"]) / sum(landing_page == "old_page")
-new_conversion_rate <- sum(converted[landing_page == "new_page"]) / sum(landing_page == "new_page")
+# Print the conversion rates
+cat("Conversion rate for the old page:", conversion_rate_old, "\n")
+cat("Conversion rate for the new page:", conversion_rate_new, "\n")
 
-# Define the prior distribution parameters
-prior_alpha <- 2
-prior_beta <- 20
 
-# Compute the posterior distributions
-old_posterior <- rbeta(10000, sum(converted[landing_page == "old_page"]) + prior_alpha, 
-                       sum(landing_page == "old_page") - sum(converted[landing_page == "old_page"]) + prior_beta)
-new_posterior <- rbeta(10000, sum(converted[landing_page == "new_page"]) + prior_alpha, 
-                       sum(landing_page == "new_page") - sum(converted[landing_page == "new_page"]) + prior_beta)
+# Task 2 - Super Kaka
 
-# Summarize the posterior distributions
-old_summary <- summary(old_posterior)
-new_summary <- summary(new_posterior)
+library(tidyverse)
+library(bayesAB)
 
-# Print the summary statistics
-print(old_summary)
-print(new_summary)
+# Number of random draws from the prior
+n_draws <- 290584
 
-hist(old_posterior)
-hist(new_posterior)
+prior <- runif(n_draws,0,1) # Here you sample n_draws draws from the prior  
+hist(prior) # It's always good to eyeball the prior to make sure it looks ok.
+
+
+# Here you define the generative model
+generative_model <- function(rate) {
+  subs=rbinom(1,size=10,prob=rate)
+  subs
+}
+
+# Here you simulate data using the parameters from the prior and the 
+# generative model
+subs <- rep(NA, n_draws)
+for(i in 1:n_draws) {
+  subs[i] <- generative_model(prior[i])
+  
+}
+
+# Here you filter off all draws that do not match the data.
+posterior <- prior[subs == 1] 
+
+hist(posterior) # Eyeball the posterior
+
+
+## Task 2 
+n_draw <- 10000
+
+# Defining and drawing from the prior distribution
+prior_rate <- runif(n_draw, 0, 1)
+
+# Defining the generative model
+gen_model <- function(rate) {
+  subscribers <- rbinom(1, size = 16, prob = rate)
+  subscribers
+}
+
+# Simulating the data
+subscribers <- rep(NA, n_draw)
+for(i in 1:n_draw) {
+  subscribers[i] <- gen_model(prior_rate[i])
+}
+
+# Filtering out those parameter values that didn't result in the
+# data that we actually observed
+post_rate <- prior_rate[subscribers == 6]
+data_list <- list(n = 16, s = 6)
+
+#### 
+install.packages("rstan")
+library(rstan)
+
+# The Stan model as a string.
+model_string <- '
+data {
+  # Number of trials
+  int 145264;
+  int 145232;
+  # Number of successes
+  int 17256;
+  int 17274; }
+
+parameters {
+  real<lower=0, upper=1> rateA;
+  real<lower=0, upper=1> rateB;
+}
+
+model {
+  rateA ~ uniform(0, 1);
+  rateB ~ uniform(0, 1);
+  sA ~ binomial(nA, rateA);
+  sB ~ binomial(nB, rateB); 
+}
+
+generated quantities {
+  real rate_diff;
+  rate_diff = rateB - rateA;
+}'
+
+
+data_list <- list(nA = 16, nB = 16, sA = 6, sB = 10)
+
+# Compiling and producing posterior samples from the model.
+stan_samples <- stan(model_code = model_string, data = data_list)
+
+# Bayes test
+
+library(bayesAB)
+
+A_binom <- rbinom(100, 1, .1203946)
+B_binom <- rbinom(100, 1, .1187448)
+AB1 <- bayesTest(A_binom, B_binom, priors = c('alpha' = 2, 'beta' = 20), distribution = 'bernoulli')
+print(AB1)
